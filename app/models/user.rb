e4491ecefec6b5dfa018992cf40e8b284a93a819
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-13 17:12:21 +0900

    バリデーションの記述変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   validates :nickname, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true
-  validates :firstname_zen, presence: true
-  validates :lastname_zen, presence: true
-  validates :firstname_kana, presence: true
-  validates :lastname_kana, presence: true
   validates :birthday, presence: true
+  with_options presence: true do
+    validates :firstname_zen, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :lastname_zen, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+  end
 
 end
