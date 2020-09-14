88495a363d2456919cbe028f4a3b1a8e56a0d4d7
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-14 20:39:23 +0900

    usersコントローラーの記述削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,2 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to controller: :items, action: :index
-    else
-      render action: :edit
-  end
 end
