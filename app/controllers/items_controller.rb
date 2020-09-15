1ebe3b3b090b5084e16ccf2bfe3d250b5d50d566
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-15 20:16:33 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
