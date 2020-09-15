21314bad6cfe79cc93516a69bf25fa9518f17418
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-15 20:26:22 +0900

    itemsテーブル作成

--- db/migrate/20200915111121_create_items.rb
+++ db/migrate/20200915111121_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string       :name,                null: false
+      t.text         :text,                null: false
+      t.string       :price,               null: false
+      t.references   :user,                null: false, foreign_key: true
+      t.integer      :category_id,         null: false
+      t.integer      :item_condition_id,   null: false
+      t.integer      :shipping_costs_id,   null: false
+      t.integer      :shipping_address_id, null: false
+      t.integer      :derivery_date_id,    null: false
       t.timestamps
     end
   end
