ae6fce89957ca0e620f17d605ad95c426a89149d
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-16 20:41:43 +0900

    商品出品画面への遷移　記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+
   def create
     @item = Item.new(item_params)
     @item.save
@@ -10,7 +14,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :image, :price, :category_id, :item_condition_id, :shipping_costs_id, :shipping_address_id, :derivery_date_id).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :items, only: [:index, :new, :create]
+
 end
