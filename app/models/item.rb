4f05601ea00cefc4d4d30f893e15d2a5742503c6
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-17 21:54:33 +0900

    itemモデル　バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,20 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :item_condition, :shipping_costs, :shipping_address, :derivery_date
   has_one_attached :image
+  belongs_to :user 
+  has_many :comments 
+  has_one :pay
+
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, messages:"半角数字のみを使ってください"}
+  validates :user, presence: true, foreign_key: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :shipping_costs_id, presence: true
+  validates :shipping_address_id, presence: true
+  validates :derivery_date_id, presence: true
 
   #空の投稿を保存できないようにする
   validates :title, :text, :category, presence: true
