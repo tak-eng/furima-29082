c0ed87904d775fc1e7bcde92ce319d274f2a6bba
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-19 10:37:17 +0900

    itemモデルテストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::App.name}
+    text {Faker::Company.name}
+    price {Faker::Number.number(digits: 6)}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('public/images/test_image.png')
+    end
+
+    it "imageとname、text、price、user、category_id、item_condition_id、shipping_costs_id、shipping_address_id、derivery_date_idが存在すれば登録できること" do
+      expect(@item).to be_valid
+    end
+
+    it "出品画像が空では登録できないこと" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "商品名が空では登録できないこと" do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "商品説明が空では登録できないこと" do
+      @item.text = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it "価格が空では登録できないこと" do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "ログイン中のユーザーではないと登録できないこと" do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User can't be blank")
+    end
+
+    it "カテゴリーが選択されていないと登録できないこと" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category id can't be blank")
+    end
+
+    it "商品状態が選択されていないと登録できないこと" do
+      @item.item_condition_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item condition id can't be blank")
+    end
+
+    it "配送料の負担が選択されていないと登録できないこと" do
+      @item.shipping_costs_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping costs id can't be blank")
+    end
+    
+    it "発送元の地域が選択されていないと登録できないこと" do
+      @item.shipping_address_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping address id can't be blank")
+    end
+
+    it "発送日までの日数が選択されていないと登録できないこと" do
+      @item.derivery_date_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Derivery date id can't be blank")
+    end
+  end
 end
