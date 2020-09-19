2fab2c6b1cf44b80069759b7c036cb9afa7e465a
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-19 16:05:10 +0900

    インデントを整える

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,20 +3,19 @@ function price(){
    const add_tax = document.getElementById("add-tax-price");
    const profit = document.getElementById("profit");
      priceInput.addEventListener('keyup', () => {
-         const value = priceInput.value; 
+       const value = priceInput.value; 
          
        if (value >= 300 && value <= 9999999){
-         let fee = value * 0.1
-         let gains = value - fee
-         add_tax.textContent = fee;
-         profit.textContent = gains;
-     } else {
-       let fee = '-';
-       let gains = '-';
-       add_tax.textContent = fee;
-       profit.textContent = gains;
-     }
-   });
+          let fee = value * 0.1
+          let gains = value - fee
+          add_tax.textContent = fee;
+          profit.textContent = gains;}
+       else {
+          let fee = '-';
+          let gains = '-';
+          add_tax.textContent = fee;
+          profit.textContent = gains;}
+      });
   
   }
   window.addEventListener('load', price);
