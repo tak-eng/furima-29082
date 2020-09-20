8c5930c311a54d5e0cb9671e117415c8cec33176
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-20 11:32:33 +0900

    出品時の登録情報を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :text, :image, :price, :category_id, :item_condition_id, :shipping_costs_id, :shipping_address_id, :delivery_date_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :image, :price, :category_id, :item_condition_id, :shipping_cost_id, :shipping_address_id, :delivery_date_id).merge(user_id: current_user.id)
   end
 
 end
