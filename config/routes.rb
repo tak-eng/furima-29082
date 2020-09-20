9a190913029a858a268565523f5c75471bd95445
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-20 09:08:11 +0900

    ルーティング、コントローラーにshowアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
 
 end
