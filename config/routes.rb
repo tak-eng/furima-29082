a33b877bebf54078c746f5c3af22c794cf6f4d8b
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-21 14:52:31 +0900

    updateアクションを記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :new, :create, :show, :edit]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 
 end
