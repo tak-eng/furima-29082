278506916b5ee41a25b14a1796df64ace351d13d
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-21 17:22:53 +0900

    destroyアクションの記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,8 +31,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id
-       @item.destroy
+    if @item.user_id == current_user.id && @item.destroy
        redirect_to root_path
     else
       render :show
