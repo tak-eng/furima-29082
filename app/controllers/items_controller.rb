c6ac82703c58de45943d40620d04a14123356a05
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-21 17:06:27 +0900

    削除機能の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
  
   def index
     @items = Item.all.order("created_at DESC")
@@ -27,6 +27,15 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+       @item.destroy
+       redirect_to root_path
+    else
+      render :show
     end
   end
 
