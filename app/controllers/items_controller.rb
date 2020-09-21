6660849c515916f5ce871d05ef9914b5d5dcb19b
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-21 16:12:02 +0900

    エラーメッセージを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
