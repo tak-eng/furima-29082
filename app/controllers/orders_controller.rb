72c29710ffa1854070621412859892d16bbf6a39
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-22 20:18:37 +0900

    購入画面にて商品の情報が表示される

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
