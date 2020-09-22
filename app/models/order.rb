9c3db0d5b975f0b87d4327778b6d0da93ba24d0f
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-22 20:28:59 +0900

    orderモデルにバリデーションとアソシエーションを記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one :sending_address
+  
+  with_options presence: true  do
+    validates :user
+    validates :item
+  end
 end
