8c991fdff7231cd262dbfa3c426e2941923b868c
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-23 19:14:22 +0900

    ordersコントローラーファイルに記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,26 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(price: order_params[:price])
+   if @order.valid?
+     pay_item
+     @order.save
+     return redirect_to root_path
+   else
+     render 'index'
   end
 
+  private
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
 end
