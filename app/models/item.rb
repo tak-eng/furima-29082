586a88c693dd579c59e5557c33036ebd606ee0d2
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-23 18:43:10 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user 
   has_many :comments 
-  has_one :pay
+  has_one :order
 
   with_options presence: true  do
     validates :name
