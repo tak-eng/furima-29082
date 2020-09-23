a54cad0df8050369935b8953e4071a65de5d434e
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-23 19:10:26 +0900

    sending_addressのマイグレーションファイルに必要なカラムを記述

--- db/migrate/20200923100001_create_sending_addresses.rb
+++ db/migrate/20200923100001_create_sending_addresses.rb
@@ -1,7 +1,13 @@
 class CreateSendingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :sending_addresses do |t|
-
+      t.string       :post_code,    null: false
+      t.integer      :prefecture,   null: false
+      t.string       :city,         null: false
+      t.string       :house_number, null: false
+      t.string       :building_name
+      t.string       :phone_number, null: false
+      t.references   :order,        null: false, foreign_key: true
       t.timestamps
     end
   end
