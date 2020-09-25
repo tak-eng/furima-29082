bfcce224f8246f35c8fbc00076827604025824d8
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-09-25 19:46:43 +0900

    sending_addressモデルにバリデーション記入

--- app/models/sending_address.rb
+++ app/models/sending_address.rb
@@ -3,4 +3,13 @@ class SendingAddress < ApplicationRecord
   belongs_to_active_hash :shipping_address
 
   belongs_to :order
+
+  with_options presence: true  do
+    validates :post_code, inclusion: { in: "-" }
+    validates :shipping_cost_id
+    validates :city
+    validates :house_number
+    validates :phone_number, length: { maximum: 11 }
+    validates :order
+  end
 end
