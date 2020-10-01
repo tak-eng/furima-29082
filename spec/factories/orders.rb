9d9268b51b69d7813a64863bde6cfce74bc41997
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-01 19:57:37 +0900

    FactoryBot記述

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :order do
-    
+    user {"1"}
+    item {"1"}
   end
 end
--- spec/factories/sending_addresses.rb
+++ spec/factories/sending_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :sending_address do
-    
+    post_code {"123-4567"}
+    shipping_address_id {"1"}
+    city {"中央区"}
+    house_number {"青山１−１−１"}
+    building_name {"柳ビル１０１"}
+    phone_number {"09012345678"}
+    order {"1"}
   end
 end
