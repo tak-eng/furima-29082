8616133ba4aaae63bb7c2fab2d05241ef2f58f25
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-14 20:33:03 +0900

    バリデーションの移動

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,4 +1,12 @@
 class Name < ApplicationRecord
   belongs_to :user, optional: true
-  validates :firstname_zen, :lastname_zen, :firstname_kana, :lastname_kana, :birthday ,presence: true
+  validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, messages:"全角の漢字・ひらがな・カタカナのいづれかを使ってください"} do
+    validates :firstname_zen
+    validates :lastname_zen
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/,  messages:"全角カタカナを使ってください"} do
+    validates :firstname_kana
+    validates :lastname_kana    
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,7 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :birthday, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, messages:"全角の漢字・ひらがな・カタカナのいづれかを使ってください"} do
-    validates :firstname_zen
-    validates :lastname_zen
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/,  messages:"全角カタカナを使ってください"} do
-    validates :firstname_kana
-    validates :lastname_kana    
-  end
+
 
 
 end
