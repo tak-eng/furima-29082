c12261f6060db21b95fc8baed1708eec719fc1a2
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-15 18:34:43 +0900

    namesのファクトリーボット記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,10 +5,6 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 8)
     password {password}
     password_confirmation {password}
-    firstname_zen {Gimei.first.kanji}
-    lastname_zen {Gimei.last.kanji}
-    firstname_kana {Gimei.first.katakana}
-    lastname_kana {Gimei.last.katakana}
-    birthday {Faker::Date.birthday}
+
   end
 end
