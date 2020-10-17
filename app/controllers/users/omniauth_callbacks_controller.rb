e2a495e92fc268bbb60d7a61098557c59d58e47a
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-17 11:28:19 +0900

    omniauth_callbacksコントローラーにif文を追記

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,6 +14,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
    def authorization
      @user = User.from_omniauth(request.env["omniauth.auth"])
+     if @user.persisted? 
+      sign_in_and_redirect @user, event: :authentication
+    else 
+      render template: 'devise/registrations/new'
+    end
    end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
