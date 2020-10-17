54ad00a77a8717f8e855d1dff360d3a2e568f988
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-17 11:32:15 +0900

    persisted?メソッドを用いてuserモデルに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ApplicationRecord
       nickname: auth.info.name,
         email: auth.info.email
     )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
   end
 
 end
