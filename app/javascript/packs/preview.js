72ba29f8b663fffe87f54fc657c394d12bef55f4
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-18 14:51:11 +0900

    inputの中にある画像を取得する

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+
+  document.getElementById('image-list').addEventListener('change', function(e){
+    const file = e.target.files[0];
+  });
 });
