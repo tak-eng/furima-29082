376cfe2ff5212f67c0dd87025ecf36eff8a32db2
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-18 15:25:50 +0900

    inputした画像を取得する(修正)

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
 
-  document.getElementById('image-list').addEventListener('change', function(e){
-    const file = e.target.files[0];
+  document.getElementById('item-image').addEventListener('change', function(e){
+    console.log(e);
   });
 });
