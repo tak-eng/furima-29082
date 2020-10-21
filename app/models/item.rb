a508e234a55f113c19d83e4697c79f05eb830978
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-21 20:36:15 +0900

    imageモデルのアソシエーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :shipping_address
   belongs_to_active_hash :delivery_date
-  has_one_attached :image
+  has_many_attached :images
   belongs_to :user 
   has_many :comments 
   has_one :order
