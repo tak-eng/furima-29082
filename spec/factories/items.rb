14d78285094ef31f6fe5fcd49654ce0b596753e9
tak-eng <65450325+tak-eng@users.noreply.github.com>
2020-10-24 14:49:47 +0900

    itemモデルのテストコードが問題なくパスできるようにした

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -11,7 +11,7 @@ FactoryBot.define do
     delivery_date_id {"1"}
 
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      item.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
